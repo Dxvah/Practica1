188c39ba816e4599d76092d9c32845b75b485917
Dxvah <idayraperezdelgado@gmail.com>
2023-10-07 18:54:24 +0100

    problema mute unmute sonidos
    - los sonidos no se pueden mutear y desmutear

--- Practica1/Assets/Scripts/PlayerS.cs
+++ Practica1/Assets/Scripts/PlayerS.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class PlayerS : MonoBehaviour
 {
@@ -9,9 +11,29 @@ public class PlayerS : MonoBehaviour
     public float movX;
     public float movY;
     public int diamante = 0;
+    public int diamantesTotales = 5;
+    public GameObject canvasVictoria;
+    public GameObject canvasDerrota;
+    public TextMeshProUGUI timerText;
+    private float tiempoRestante = 60.0f;
+    private bool partidaTerminada = false;
+    SpriteRenderer p_SpriteRenderer;
+    public TextMeshProUGUI nDiamantes;
+    public TextMeshProUGUI cuantosDiamantes;
+    public AudioSource audioVictoria;
+    public AudioSource audioDerrota;
+    public AudioSource audiofondo;
+    private bool sonidosMuteados = false;
+
+
+    
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        canvasVictoria.SetActive(false);
+        canvasDerrota.SetActive(false);
+        p_SpriteRenderer = GetComponent<SpriteRenderer>();
+        ActualizarTextoDiamantes();
     }
 
 
@@ -19,6 +41,19 @@ public class PlayerS : MonoBehaviour
     {
         movX = Input.GetAxis("Horizontal");
         movY = Input.GetAxis("Vertical");
+        if (!partidaTerminada)
+        {
+            tiempoRestante -= Time.deltaTime;
+            timerText.text = ": " + Mathf.Round(tiempoRestante).ToString();
+
+            if (tiempoRestante <= 0)
+            {
+                tiempoRestante = 0;
+                MostrarCanvasDerrota();
+                PauseGame();
+                
+            }
+        }
     }
     void FixedUpdate()
     {
@@ -36,12 +71,74 @@ public class PlayerS : MonoBehaviour
         {
             diamante++;
             Destroy(col.gameObject);
+            ActualizarTextoDiamantes();
+            if(diamante < 4)
+            {
+                cuantosDiamantes.text = "Ooooh, vaya pena. No has conseguido ningun diamante";
+            }
+            else if (diamante >= 4 && diamante < diamantesTotales)
+            {
+                cuantosDiamantes.text = "¡Has terminado! Aún así, te has dejado algunos diamantes por el camino";
+            }
+            else 
+            {
+                cuantosDiamantes.text = "Perfect! Has conseguido todos los diamantes ¡Felicidades!";
+            }
 
         }
         else if(col.gameObject.tag == "Meta")
         {
+            MostrarCanvasVictoria();
             PauseGame();
             Destroy(col.gameObject);
         }
+        else if (col.gameObject.tag == "Rotatorio")
+        {
+              GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+              Invoke("ReactivarMovimiento", 2f);
+              p_SpriteRenderer.color = Color.red;
+        }
+         else if (col.gameObject.tag == "Pinchos")
+        {
+              GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeAll;
+              Invoke("ReactivarMovimiento", 3f);
+              p_SpriteRenderer.color = Color.red;
+        }
+    }
+    void ReactivarMovimiento()
+    {   
+    
+        GetComponent<Rigidbody2D>().constraints = RigidbodyConstraints2D.FreezeRotation;
+        p_SpriteRenderer.color = Color.white;
+    
+    
+    }
+    void MostrarCanvasVictoria()
+    {
+        canvasVictoria.SetActive(true);
+        if (audiofondo != null && audioVictoria != null && sonidosMuteados)
+        {
+            audiofondo.UnPause();
+            audioVictoria.UnPause();
+            sonidosMuteados = false;
+        }
+        
+    }
+
+    void MostrarCanvasDerrota()
+    {
+        canvasDerrota.SetActive(true);
+        if (audiofondo != null && audioDerrota != null && sonidosMuteados)
+        {
+            audiofondo.UnPause();
+            audioDerrota.UnPause();
+            sonidosMuteados = false;
+        }
+        
+  
+    }
+    private void ActualizarTextoDiamantes()
+    {
+        nDiamantes.text = ": " + diamante + " / " + diamantesTotales;
     }
 }
